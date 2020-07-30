44d19701d7f437774a559e99ba7e0e77a75806ee
Kyrylo Garlinsjky <kyrylo.garlinsjkyj@renome.ua>
2020-07-30 18:02:52 +0300

    changes

--- my-app/src/components/Content/Dialogs/ListDialogs/ListDialogs.jsx
+++ my-app/src/components/Content/Dialogs/ListDialogs/ListDialogs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "../Dialogs.module.css"
+import Interlocutor from "./Interlocutor/Interlocutor"
 
 const ListDialogs = () => {
     return (
@@ -14,20 +15,9 @@ const ListDialogs = () => {
                 </div>
                 <div className={s.messagesList}>
                     <ul>
-                        <li className={s.active}>
-                            <div className={s.usrMsgDetails}>
-                                <div className={s.usrMsImg}>
-                                    <img src={"./img/user.png"} alt="User"/>
-                                    <span className={s.msgStatus}/>
-                                </div>
-                                <div className={s.usrMgInfo}>
-                                    <h3>John Doe</h3>
-                                    <p>Lorem ipsum dolor</p>
-                                </div>
-                                <span className={s.postedTime}>1:55 PM</span>
-                                <span className={s.msgNotifc}>1</span>
-                            </div>
-                        </li>
+                        <Interlocutor />
+                        <Interlocutor />
+                        <Interlocutor />
                     </ul>
                 </div>
             </div>
