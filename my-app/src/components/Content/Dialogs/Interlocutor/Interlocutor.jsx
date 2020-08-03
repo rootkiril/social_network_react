fdaf2e3cfa664decb72f01445860f823009bb149
Kyrylo Garlinsjky <kyrylo.garlinsjkyj@renome.ua>
2020-08-03 10:42:25 +0300

    refactoring

---
+++ my-app/src/components/Content/Dialogs/Interlocutor/Interlocutor.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "../Dialogs.module.css";
+
+
+
+const Interlocutor = (props) => {
+  return (
+    <li>
+      <NavLink
+        to={"/dialogs/" + props.id}
+        className={s.usrMsgDetails}
+        activeClassName={s.active}
+      >
+        <div className={s.usrMsImg}>
+          <img src={"./img/user.png"} alt="User" />
+          <span className={s.msgStatus} />
+        </div>
+        <div className={s.usrMgInfo}>
+          <h3>{props.username}</h3>
+          <p>Last message blabla</p>
+        </div>
+        <span className={s.postedTime}>1:55 PM</span>
+        <span className={s.msgNotifc}>1</span>
+      </NavLink>
+    </li>
+  );
+};
+
+export default Interlocutor;
---
+++ my-app/src/components/Content/Dialogs/Message/Message.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import s from "../Dialogs.module.css";
+
+const MessageBox = (props) => {
+    return (
+      <div className={s.mainMessageBox + " " + s.taRight}>
+        <div className={s.messageDt}>
+          <div className={s.messageInnerDt}>
+            <p>{props.message}</p>
+          </div>
+          <span>{props.date}</span>
+        </div>
+        <div className={s.messgUsrImg}>
+          <img src={"./img/user.png"} alt="Avatar" />
+        </div>
+      </div>
+    );
+  };
+
+  export default MessageBox
